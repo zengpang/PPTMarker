a4fd8e8e18ac73890a8ab96085e00369a3481955
zengpang <2516629927@qq.com>
2022-08-18 23:34:04 +0800

    更新

--- PageJs/index.js
+++ PageJs/index.js
@@ -1,28 +1,29 @@
-const isMain=str=>(/^#{1,2}(?!#)/).test(str);
-const isSub=str=>(/^#{3}(?!#)/).test(str);
-function convert(raw){
-    let arr=raw.split(/\n(?=\s*#)/).filter(s=>s!="").map(s=>s.trim());//根据#号分割用户输入内容
-    let html=``;
+const $ = s => document.querySelector(s);
+const $$ = s => document.querySelectorAll(s);
+const isMain = str => (/^#{1,2}(?!#)/).test(str);
+const isSub = str => (/^#{3}(?!#)/).test(str);
+
+function convert(raw) {
+    let arr = raw.split(/\n(?=\s*#)/).filter(s => s != "").map(s => s.trim());//根据#号分割用户输入内容
+    let html = ``;
     //遍历用户输入
-    for(let i=0;i<arr.length;i++)
-    {
+    for (let i = 0; i < arr.length; i++) {
         //判断下一行是否为空
-        if(arr[i+1]!==undefined){
-            switch(true)
-            {
-                case(isMain(arr[i])&&isMain(arr[i+1])):
-                {
-                    html+=`
+        if (arr[i + 1] !== undefined) {
+            switch (true) {
+                case (isMain(arr[i]) && isMain(arr[i + 1])):
+                    {
+                        html += `
                     <section data-markdown>
                     <textarea data-template>
                     ${arr[i]}
                     </textarea>
                     </section>
                     `
-                };break;
-                case(isMain(arr[i])&&isSub(arr[i+1])):
-                {
-                    html+=`
+                    }; break;
+                case (isMain(arr[i]) && isSub(arr[i + 1])):
+                    {
+                        html += `
                     <section>
                     <section data-markdown>
                     <textarea data-template>
@@ -30,20 +31,20 @@ function convert(raw){
                     </textarea>
                     </section>
                     `
-                };break;
-                case(isSub(arr[i])&&isSub(arr[i+1])):
-                {
-                    html+=`
+                    }; break;
+                case (isSub(arr[i]) && isSub(arr[i + 1])):
+                    {
+                        html += `
                     <section data-markdown>
                     <textarea data-template>
                     ${arr[i]}
                     </textarea>
                     </section>
                     `
-                };break;
-                case(isSub(arr[i])&&isMain(arr[i+1])):
-                {
-                    html+=`
+                    }; break;
+                case (isSub(arr[i]) && isMain(arr[i + 1])):
+                    {
+                        html += `
                     <section data-markdown>
                     <textarea data-template>
                     ${arr[i]}
@@ -51,26 +52,25 @@ function convert(raw){
                     </section>
                     </section>
                     `
-                };break;
+                    }; break;
             }
         }
         else //如果为空判断遍历到最后一行
         {
-             switch(true)
-             {
-                case(isMain(arr[i])):
-                {
-                    html+=`
+            switch (true) {
+                case (isMain(arr[i])):
+                    {
+                        html += `
                     <section data-markdown>
                     <textarea data-template>
                     ${arr[i]}
                     </textarea>
                     </section>
                     `
-                };break;
-                case(isSub(arr[i])):
-                {
-                    html+=`
+                    }; break;
+                case (isSub(arr[i])):
+                    {
+                        html += `
                     <section data-markdown>
                     <textarea data-template>
                     ${arr[i]}
@@ -78,27 +78,84 @@ function convert(raw){
                     </section>
                     </section>
                     `
-                };break;
-             }
+                    }; break;
+            }
         }
     }
     return html;
 }
-function loadMarkdown(raw)
-{
-    localStorage.markdown=raw;
-    location.reload();
-}
-//PPT初始化
-function start(){
-    let TPL=`## 欢迎使用PPTMarker`//初始PPT语句
-    let html=convert(localStorage.markdown||TPL);//检测是否存在localStorage.markdown，如果有则传入localStorage.markdown内容，否则传入初始语句
-    document.querySelector(`.slides`).innerHTML=html;
-    Reveal.initialize({
-        hash: true,
+//菜单栏
+const Menu = {
+    init() {
+        console.log(`菜单初始化`);
+        this.$settingIcon = $(`.control .icon-shezhi`);
+        this.$menu = $(`.menu`);
+        this.$closeIcon = $(`.menu .icon-guanbi`);
+        this.$$tabs = $$(`.menu .tab`);
+        this.$$contents = $$(`.menu .content`);
+        this.bind();
+    },
+    //事件绑定
+    bind() {
+        //设置按钮点击事件
+        this.$settingIcon.onclick=()=>{
+            this.$menu.classList.add(`open`);
+        }
+        //关闭按钮点击事件
+        this.$closeIcon.onclick=()=>{
+            this.$menu.classList.remove(`open`);
+        }
+        //
+        this.$$tabs.forEach($tab => $tab.onclick = () => {
+            this.$$tabs.forEach($node => $node.classList.remove(`active`));
+            $tab.classList.add(`active`);
+            let index = [...this.$$tabs].indexOf($tab);
+            this.$$contents.forEach($node => $node.classList.remove(`active`));
+            this.$$contents[index].classList.add(`active`);
+        })
 
-        // Learn about plugins: https://revealjs.com/plugins/
-        plugins: [RevealMarkdown, RevealHighlight, RevealNotes]
-    });
+    }
 }
-start();//每次页面刷新调用
+//编辑器
+const Editor = {
+    init() {
+        console.log(`编译器初始化`);
+        this.$editInput = $(`.editor textarea`);
+        this.$saveBtn = $(`.editor .button-save`);
+        this.$slideContainer = $(`.slides`);
+        let TPL = `## 欢迎使用PPTMarker`;//初始PPT语句
+        this.markdown = localStorage.markdown || TPL;//检测是否存在localStorage.markdown，如果有则传入localStorage.markdown内容，否则传入初始语句
+        this.bind();
+        this.start();
+    },
+    bind(){
+        //保存按钮点击事件
+        this.$saveBtn.onclick=()=>{
+            localStorage.markdown=this.$editInput.value;
+            location.reload();
+        }
+    },
+    //PPT初始化
+    start() {
+        this.$editInput.value=this.markdown;
+        this.$slideContainer.innerHTML=convert(this.markdown);
+        Reveal.initialize({
+            hash: true,
+
+            // Learn about plugins: https://revealjs.com/plugins/
+            plugins: [RevealMarkdown, RevealHighlight, RevealNotes]
+        });
+    }
+}
+const App = {
+    init() {
+      [...arguments].forEach(Modlue=>Modlue.init());
+    }
+}
+App.init(Menu,Editor);
+// function loadMarkdown(raw) {
+//     localStorage.markdown = raw;
+//     location.reload();
+// }
+
+// start();//每次页面刷新调用
